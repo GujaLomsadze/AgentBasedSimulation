072f0aa0525ff539c9e416921c34469828619469
lomsadze <lomsadze.guja@gmail.com>
2024-02-18 13:27:50 +0100

    ADD: Nothing much

--- entities/nodes/classes.py
+++ entities/nodes/classes.py
@@ -5,17 +5,22 @@ from typing import Dict, Optional
 @dataclass
 class Node:
     name: str
-    type: str
-    ip: str = None
-    port: int = None
-    cpu_usage: float = 0.0  # Percentage
-    ram_usage: float = 0.0  # GB
-    disk_usage: float = 0.0  # GB
-    disk_capacity: int = 50  # GB
+    type: Optional[str]
+    ip: Optional[str] = None
+    port: Optional[int] = None
+
+    cpu_usage: Optional[float] = 0.0  # Percentage
+    ram_usage: Optional[float] = 0.0  # GB
+    disk_usage: Optional[float] = 0.0  # GB
+
+    disk_capacity: Optional[int] = 50  # GB
+
     rps: Optional[float] = 1_000  # Requests Per Second
     qps: Optional[float] = 1_000  # Queries Per Second
     tps: Optional[float] = 1_000  # Transactions Per Second
+
     retention_time_hours: Optional[int] = None  # Retention time in hours
+
     additional_metrics: Dict[str, float] = field(default_factory=dict)
 
 
@@ -47,3 +52,10 @@ class RedisInstance(Node):
 class MongoDBInstance(Node):
     type: str = "MongoDBInstance"
     collections_count: Optional[int] = None
+
+
+@dataclass
+class KafkaInstance(Node):
+    type: str = "KafkaInstance"
+    topics_count: Optional[int] = None
+    topics_partitions: Optional[int] = None
--- main.py
+++ main.py
@@ -5,22 +5,5 @@ adjacency_matrix = json_to_matrix("data/nodes.json")
 
 # get_graph_stats(adjacency_matrix=adjacency_matrix)
 
-pg1 = PostgreSQLInstance(
-    type="PostgreSQLInstance",
-    name="PG_1",
-    ip="10.0.55.200",
-    port=5432,
-    cpu_usage=20,
-    active_connections=1
-)
-
-ms1 = NodejsService(
-    type="Microservice N1",
-    name="NS_1",
-    ip="10.0.55.201",
-    port=5432,
-    cpu_usage=20,
-)
-
-
+pg?
 
